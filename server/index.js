e22a4bcb7d2552730b5d755658f17453ad9435ea
Pires <daniel.pires@hp.com>
2021-07-22 10:40:28 -0300

    Removing email sendind feature, removing not needed libs

--- server/index.js
+++ server/index.js
@@ -1,25 +1,17 @@
-(function() {
+const express = require('express');
+const router = express.Router();
 
-  'use strict';
-  const express = require('express');
-  const router = express.Router();
-  const emailService = require('./controllers/mail.controller.js')
+router.get('/caderno1', (req, res) => {
+  res.contentType("application/pdf");
+  res.status(200).sendFile(__dirname + '/cadernos/CADERNO1.pdf');
+});
+router.get('/caderno2', (req, res) => {
+  res.contentType("application/pdf");
+  res.status(200).sendFile(__dirname + '/cadernos/CADERNO2.pdf');
+});
+router.get('/caderno3', (req, res) => {
+  res.contentType("application/pdf");
+  res.status(200).sendFile(__dirname + '/cadernos/CADERNO3.pdf');
+});
 
-  router.get('/caderno1', (req, res) => {
-    res.contentType("application/pdf");
-    res.status(200).sendFile(__dirname + '/cadernos/CADERNO1.pdf');
-  });
-  router.get('/caderno2', (req, res) => {
-    res.contentType("application/pdf");
-    res.status(200).sendFile(__dirname + '/cadernos/CADERNO2.pdf');
-  });
-  router.get('/caderno3', (req, res) => {
-    res.contentType("application/pdf");
-    res.status(200).sendFile(__dirname + '/cadernos/CADERNO3.pdf');
-  });
-  router.post('/sendmail', emailService.sendEmail);
-
-  module.exports = router;
-
-
-}());
+module.exports = router;
